d1e09e2c5b9d2b81e62dda900c24b77d1da2811c
MANYA SHUKLA <shuklamanya99@gmail.com>
2025-09-23 11:52:21 +0530

    Added navbar component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,58 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.motor.es',
+      },
+      {
+        protocol: 'https',
+        hostname: 's.yimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.bmw.in',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.autocarindia.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.bigboytoyz.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.team-bhp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.cdn.newsbytesapp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'motorz.me',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'bmw.scene7.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
